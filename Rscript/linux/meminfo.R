f4cf28e1ec4c249e863e38e23aa44f2b5d7e9315
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-22 15:05:05 +0800

    run meminfo test success

--- Rscript/linux/meminfo.R
+++ Rscript/linux/meminfo.R
@@ -1,7 +1,12 @@
 imports "linux" from "Profiler";
 
-"D:\linux-profiler\Rscript\proc\meminfo.txt"
+let summary = "D:\linux-profiler\Rscript\proc\meminfo.txt"
 :> readText
 :> meminfo
-:> str
+:> as.list
 ;
+
+print("Active file:");
+print(summary[["Active(file)"]]);
+
+str(summary);

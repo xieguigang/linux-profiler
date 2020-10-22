ac67ad475249a23f358014620399d9a207096f66
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-22 14:16:56 +0800

    meminfo

--- Profiler/ProfilerReport.vb
+++ Profiler/ProfilerReport.vb
@@ -1,3 +1,12 @@
-﻿Public Class ProfilerReport
+﻿Imports Profiler.Commands
+
+Public Class ProfilerReport
+
+    ''' <summary>
+    ''' system load and timespan summary
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property uptime As uptime
+
 
 End Class
---
+++ Profiler/proc/meminfo.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Data.Linq.Mapping
+
+Namespace proc
+
+    Public Class meminfo
+
+        Public Property MemTotal As Double
+        Public Property MemFree As Double
+        Public Property MemAvailable As Double
+        Public Property Buffers As Double
+        Public Property Cached As Double
+        Public Property SwapCached As Double
+        Public Property Active As Double
+        Public Property Inactive As Double
+        <Column(Name:="Active(anon)")>
+        Public Property Active_anon As Double
+        <Column(Name:="Inactive(anon)")>
+        Public Property Inactive_anon As Double
+        <Column(Name:="Active(file)")>
+        Public Property Active_file As Double
+        <Column(Name:="Inactive(file)")>
+        Public Property Inactive_file As Double
+        Public Property Unevictable As Double
+        Public Property Mlocked As Double
+        Public Property SwapTotal As Double
+        Public Property SwapFree As Double
+        Public Property Dirty As Double
+        Public Property Writeback As Double
+        Public Property AnonPages As Double
+        Public Property Mapped As Double
+        Public Property Shmem As Double
+        Public Property Slab As Double
+        Public Property SReclaimable As Double
+        Public Property SUnreclaim As Double
+        Public Property KernelStack As Double
+        Public Property PageTables As Double
+        Public Property NFS_Unstable As Double
+        Public Property Bounce As Double
+        Public Property WritebackTmp As Double
+        Public Property CommitLimit As Double
+        Public Property Committed_AS As Double
+        Public Property VmallocTotal As Double
+        Public Property VmallocUsed As Double
+        Public Property VmallocChunk As Double
+        Public Property HardwareCorrupted As Double
+        Public Property AnonHugePages As Double
+        Public Property CmaTotal As Double
+        Public Property CmaFree As Double
+        Public Property HugePages_Total As Double
+        Public Property HugePages_Free As Double
+        Public Property HugePages_Rsvd As Double
+        Public Property HugePages_Surp As Double
+        Public Property Hugepagesize As Double
+        Public Property DirectMap4k As Double
+        Public Property DirectMap2M As Double
+        Public Property DirectMap1G As Double
+
+        Public Shared Function Parse(stdout As String) As meminfo
+
+        End Function
+
+    End Class
+End Namespace

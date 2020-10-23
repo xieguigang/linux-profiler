e3a30d055d88dd2da3f0b113acd4b1908d02bf63
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-23 15:04:41 +0800

    Update Report.vb

--- Profiler/Rscript/Report.vb
+++ Profiler/Rscript/Report.vb
@@ -9,6 +9,7 @@ Module Report
 
     <ExportAPI("start.session")>
     Public Function start_session(profiler As Profiler) As Profiler
+        Call "Linux system performance profiler session is started, press Ctrl + C for stop.".__INFO_ECHO
         Call profiler.Run()
         Return profiler
     End Function

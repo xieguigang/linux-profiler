c69648c345ff506d1f63366db552992221ab73ff
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-23 14:12:30 +0800

    /etc/os-release

---
+++ Profiler/Files/etc/os_release.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Data.Linq.Mapping
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop.CType
+
+Namespace etc
+
+    ''' <summary>
+    ''' cat /etc/os-release
+    ''' </summary>
+    Public Class os_release : Implements ICTypeList
+
+        Public Property NAME As String
+        Public Property VERSION As String
+        Public Property ID As String
+        Public Property ID_LIKE As String
+        Public Property VERSION_ID As String
+        Public Property PRETTY_NAME As String
+        Public Property ANSI_COLOR As String
+        Public Property CPE_NAME As String
+        Public Property HOME_URL As String
+        Public Property BUG_REPORT_URL As String
+
+        Public Property metadata As Dictionary(Of String, String)
+
+        Public Function toList() As list Implements ICTypeList.toList
+            Return New list With {
+                .slots = New Dictionary(Of String, Object) From {
+                    {"NAME", NAME},
+                    {"VERSION", VERSION},
+                    {"ID", ID},
+                    {"ID_LIKE", ID_LIKE},
+                    {"VERSION_ID", VERSION_ID},
+                    {"PRETTY_NAME", PRETTY_NAME},
+                    {"ANSI_COLOR", ANSI_COLOR},
+                    {"CPE_NAME", CPE_NAME},
+                    {"HOME_URL", HOME_URL},
+                    {"BUG_REPORT_URL", BUG_REPORT_URL},
+                    {"metadata", New list With {
+                        .slots = metadata _
+                            .ToDictionary(Function(t) t.Key,
+                                          Function(t)
+                                              Return CObj(t.Value)
+                                          End Function)
+                        }
+                    }
+                }
+            }
+        End Function
+
+        Friend Shared Function Parse(stdout As String) As os_release
+            Dim lines As String()() = stdout _
+                .LineTokens _
+                .Split(Function(line) line.StringEmpty, includes:=False)
+            Dim writer = Schema(Of ColumnAttribute).GetSchema(GetType(os_release), Function(c) c.Name, explict:=True)
+            Dim info As New os_release
+
+            For Each tag As NamedValue(Of String) In lines(Scan0) _
+                .Select(Function(line)
+                            Return line.GetTagValue("=", trim:=""""c)
+                        End Function)
+
+                Call writer.Write(tag.Name, info, tag.Value)
+            Next
+
+            info.metadata = lines(1) _
+                .Select(Function(line)
+                            Return line.GetTagValue("=", trim:=""""c)
+                        End Function) _
+                .ToDictionary(Function(m) m.Name,
+                              Function(m)
+                                  Return m.Value
+                              End Function)
+
+            Return info
+        End Function
+    End Class
+End Namespace

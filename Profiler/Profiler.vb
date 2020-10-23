97cc9693519af5d93d5239a614ec1e05477d7cad
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-23 09:41:36 +0800

    profiler module

---
+++ Profiler/Profiler.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Threading
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.Utility
+Imports Microsoft.VisualBasic.ComponentModel
+
+Public Class Profiler : Implements ITaskDriver
+
+    Dim cancel As Boolean = False
+    Dim save As String
+    Dim seconds As Integer
+
+    Sub New(save As String, Optional seconds As Integer = 15)
+        Me.save = save
+        Me.cancel = False
+        Me.seconds = seconds
+    End Sub
+
+    Public Function Run() As Integer Implements ITaskDriver.Run
+        Dim cancel As New UserTaskCancelAction(Sub() Me.cancel = True)
+
+        Do While Not Me.cancel
+            Call sampling()
+            Call Thread.Sleep(seconds * 1000)
+        Loop
+
+        Return flush()
+    End Function
+
+    Private Function flush() As Integer
+
+    End Function
+
+    Private Sub sampling()
+
+    End Sub
+End Class

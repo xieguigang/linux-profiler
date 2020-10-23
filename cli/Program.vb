4cc7949eae2a2267966d60ac9be367221ef2cfb0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-23 15:41:32 +0800

    add cli module for run profiler sampling

--- cli/Program.vb
+++ cli/Program.vb
@@ -1,7 +1,20 @@
-﻿Module Program
+﻿Imports Linux
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
-    Sub Main()
+Module Program
 
-    End Sub
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/run")>
+    <Usage("/run /save <samples.zip> [/interval <seconds, default=15>]")>
+    Public Function RunProfiler(args As CommandLine) As Integer
+        Dim save$ = args <= "/save"
+        Dim interval% = args("/interval") Or 15
+
+        Return New Profiler(save, interval).Run
+    End Function
 
 End Module
